70c7ad9329e3860f7fcbcf78f3f23c730ed3994e
Aaron Corson <chaopivoo@live.com>
2021-08-22 17:08:41 -0700

    checked out css rendering using 3js

--- components/Dice-Roller/Dice-Roller-Lg/Component.tsx
+++ components/Dice-Roller/Dice-Roller-Lg/Component.tsx
@@ -7,9 +7,12 @@ import {
 	BoxGeometry,
 	MeshBasicMaterial,
 	Mesh,
-	sRGBEncoding
+	sRGBEncoding,
+	Color//,
+	//CSS3DObject
 } from 'three';
 import { GLTFLoader } from '../../../node_modules/three/examples/jsm/loaders/GLTFLoader.js';
+import { CSS3DRenderer, CSS3DObject} from '../../../node_modules/three/examples/jsm/renderers/CSS3DRenderer.js';
 import gsap from 'gsap'
 //import * as THREE from 'three';
 interface props {
@@ -25,7 +28,7 @@ export default class DiceRoller extends Component<props,state>{
 	scene:Scene
 	cube:Mesh
 	camera:PerspectiveCamera
-	renderer:WebGLRenderer
+	renderer:any
 	loader:GLTFLoader
 	dice:any
 
@@ -41,15 +44,17 @@ export default class DiceRoller extends Component<props,state>{
 	componentDidMount():void {
 		this.loader = new GLTFLoader();
 		this.setScene();
+		//this.positionCamera(500,500,500)
+		this.createCSSContent();
 		this.createCube();
-		this.createD20();
+		//this.createD20();
 		this.update();
 		window.addEventListener("resize",this.resize.bind(this))
 	}
 
 
 	resize(){
-		this.renderer.setSize(window.innerWidth/2, window.innerHeight/2);
+		this.renderer.setSize(window.innerWidth, window.innerHeight);
 	}
 
 	createD20(){
@@ -99,6 +104,50 @@ export default class DiceRoller extends Component<props,state>{
 		},2000)
 	}
 
+	positionCamera(x,y,z){
+		x && (this.camera.position.x = x);
+		y && (this.camera.position.y = y);
+		z && (this.camera.position.z = z);
+		this.camera.lookAt(this.scene.position);
+	}
+
+	testContent(){
+		return  `
+		<div style="width:370px;height:370px;opacity:0.5;background-color:blue;">
+			<h1>This is an H1 Element.</h1>
+			<span class="large">Hello Three.js cookbook</span>
+			<textarea> And this is a textarea</textarea>
+	  	</div>`;
+	}
+
+	cssElement:any
+	createCSSContent(){
+		this.cssElement = this.createCSS3DObject(this.testContent());
+		this.cssElement.position.set(100, 100, 100);
+		this.scene.add(this.cssElement);
+	}
+
+	createCSS3DObject(content) 
+    {
+      // convert the string to dome elements
+      var wrapper = document.createElement('div');
+      wrapper.innerHTML = content;
+      var div = wrapper.firstChild;
+
+      // set some values on the div to style it.
+      // normally you do this directly in HTML and 
+      // CSS files.
+	  console.log(div, wrapper);
+      //div.style.width = '370px';
+      //div.style.height = '370px';
+      //div.style.opacity = 0.7;
+      //div.style.background = new Color(Math.random() * 0xffffff).getStyle();
+
+      // create a CSS3Dobject and return it.
+      var object = new CSS3DObject(wrapper);
+      return object;
+    }
+
 	update():void {
 		requestAnimationFrame(this.update.bind(this));
 
@@ -120,16 +169,17 @@ export default class DiceRoller extends Component<props,state>{
 
 	setScene():void{
 		this.scene = new Scene();
-		this.camera = new PerspectiveCamera( 75, (window.innerWidth/2)/(window.innerHeight/2), 0.1, 1000 );
+		this.camera = new PerspectiveCamera( 75, (window.innerWidth)/(window.innerHeight), 0.1, 2000 );
 
 		this.renderer = new WebGLRenderer();
-		this.renderer.outputEncoding = sRGBEncoding;
-		this.renderer.setSize( window.innerWidth/2, window.innerHeight/2 );
+		//this.renderer.outputEncoding = sRGBEncoding;
+		this.renderer.setSize( window.innerWidth, window.innerHeight);
+		//this.renderer.setClearColor( 0xffffff, 0.5);
 		this.element.current.appendChild( this.renderer.domElement );
 	}
 
 	render():ReactNode {
-		return <div ref={this.element}></div>
+		return <div ref={this.element} style={{position:'absolute'}}></div>
 	}
 
 	static roll(faces:number):number{
--- pages/index.tsx
+++ pages/index.tsx
@@ -22,11 +22,9 @@ export default class Home extends Component<props,state>{
   }
   //console.log(styles)
   render():ReactNode{
-    
+
     return <div>
-      <DiceRoller/>
-    </div>
-    return <div>
+    <DiceRoller/>
     <Paths data={[{start:{x:0,y:0},end:{x:100,y:100}}]}></Paths>
     <Paths data={[{start:{x:100,y:100},end:{x:200,y:200}}]}></Paths>
     <Editor/>
